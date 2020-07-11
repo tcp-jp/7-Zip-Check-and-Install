582a90f46947f8f3319e0b8a9a21127ab8798fd8
Joe Paterson <66903243+tcp-jp@users.noreply.github.com>
2020-07-11 11:53:20 +0100

    Declared Variables
    Declared variables and included examples to be replaced on use.

--- 7-ZipInstaller.bat
+++ 7-ZipInstaller.bat
@@ -1,3 +1,7 @@
+set installPath="Set where you want the 7-Zip install exe to go -- DOES NOT MAKE PATH FOR YOU"
+set sevenzipURL="Set this to the 7-Zip exe link you require -- i.e. "https://www.7-zip.org/a/7z1900.exe" <-- 32bit Windows"
+
+
 :sevenzip
 dir "%programfiles(x86)%" | find /i "7-zip" && set path=%path%;%programfiles(x86)%\7-Zip\; && goto :eof
 dir "%programfiles%" | find /i "7-zip" && set path=%path%;%programfiles%\7-Zip\; && goto :eof
